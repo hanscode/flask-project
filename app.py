3c82a901875bfbca2b5300154fc33776292ccbbc
Hans Steffens <hans@steffens.co>
2025-05-05 18:58:24 -0500

    Add and View Pets

--- app.py
+++ app.py
@@ -1,23 +1,47 @@
-from flask import Flask, render_template, url_for, request
+from flask import (render_template, url_for, request, redirect)
 
-app = Flask(__name__)
+from models import db, Pet, app
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    pets = Pet.query.all()
+    return render_template('index.html', pets=pets)
 
 
 @app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
-    print(request.form)
+    if request.form:
+        print(request.form)
+        print(request.form['name'])
+        new_pet = Pet(
+            name=request.form['name'],
+            age=request.form['age'],
+            breed=request.form['breed'],
+            color=request.form['color'],
+            size=request.form['size'],
+            weight=request.form['weight'],
+            url=request.form['url'],
+            url_tag=request.form['alt'],
+            pet_type=request.form['pet'],
+            gender=request.form['gender'],
+            spay=request.form['spay'],
+            house_trained=request.form['housetrained'],
+            description=request.form['description']
+        )
+        db.session.add(new_pet)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('addpet.html')
 
 
-@app.route('/pet')
-def pet():
-    return render_template('pet.html')
+@app.route('/pet/<id>')
+def pet(id):
+    pet = Pet.query.get(id)
+    return render_template('pet.html', pet=pet)
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=8000, host='127.0.1')
