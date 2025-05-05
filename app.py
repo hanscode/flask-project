56d5d1eb1fb538478525cf56eb6d16a7a54b849a
Hans Steffens <hans@steffens.co>
2025-05-05 15:26:22 -0500

    Template Inheritance and form

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return "Hello from Pet adoption app!"
+    return render_template('index.html')
 
-# create a meow route
-@app.route('/meow')
-def meow():
-    return "Meow!"
+
+@app.route('/add-pet', methods=['GET', 'POST'])
+def add_pet():
+    print(request.form)
+    return render_template('addpet.html')
+
+
+@app.route('/pet')
+def pet():
+    return render_template('pet.html')
 
 
 if __name__ == '__main__':
