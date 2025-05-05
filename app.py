f90c88c9a1d251f1ea5e88ca09bb0881ab521e4c
Hans Steffens <hans@steffens.co>
2025-05-05 13:27:04 -0500

    Creating the initial flask application

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello from Pet adoption app!"
+
+# create a meow route
+@app.route('/meow')
+def meow():
+    return "Meow!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000, host='127.0.1')
